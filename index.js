a898fd0eb531ea9876ebe7a810a59457b438398e
Dear Bear <dearerbear@gmail.com>
2018-08-09 02:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var recipes = new Object()
 console.log(recipes)
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-  return object;
+  // object[key] = value;
+  // return object;
+  newObject = Object.assign({}, object, {key: "test"})
+  console.log(newObject);
+  return newObject
 }
